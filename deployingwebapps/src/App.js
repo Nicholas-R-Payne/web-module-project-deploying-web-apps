f9ee23ffe71dd70a3b803f985600b87066eeb6e7
DarkedgeTH <darkedgeth@gmail.com>
2022-03-31 16:23:40 -0400

    added counter from previous project

--- deployingwebapps/src/App.js
+++ deployingwebapps/src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+    /* STEP 1 */
+    const [count, setCount] = useState(0);
+
+    const increment = () => {
+      /* STEP 4 */
+      setCount(count + 1);
+    };
+    const decrement = () => {
+      /* STEP 5 */
+      setCount(count - 1);
+    };
+    const reset = () => {
+      /* STEP 6 */
+      setCount(0);
+    };
+  
+    const style = {
+      fontSize: '1.5em',
+      marginBottom: '0.3em',
+      color: count % 2 === 0 ? 'royalblue' : 'crimson' /* STEP 2 */
+    };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='widget-counter container'>
+      <h2>Counter</h2>
+      <div id='count' style={style}>
+        Number {count} is {count % 2 === 0 ? 'even' : 'odd'} {/* STEP 3 */}
+      </div>
+      <div>
+        <button id='increment' onClick={increment}>Increment</button>
+        <button id='decrement' onClick={decrement}>Decrement</button>
+        <button id='resetCount' onClick={reset}>Reset</button>
+      </div>
     </div>
   );
 }
